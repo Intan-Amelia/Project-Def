c63b8ea10936c2e49e3273efa3ead0103e4ca52a
Theresia028 <83936446+Theresia028@users.noreply.github.com>
2021-06-01 20:26:06 +0700

    upload file menu.js

---
+++ 03_menu.js
@@ -0,0 +1,6 @@
+
+var makanan = ['Nasi Kuning Tumpeng Mini','Nasi Rames','Brekecek','Gado-Gado','Karedok','Mangut Beong','Nasi Jamblang','Nasi Gandul','Nasi Grombyang','Nasi Lengko',
+'Nasi Liwet', 'Nasi Tumpang', 'Rawon','Cah Kangkung','Soto','Tahu Gejrot','Tauge Goreng','Tempe Mendoan'];
+var minuman = ['Es Buah','Es Cendol','Bajigur','Es Doger','Es Goyobod','Jamu Beras Kencur','Wedang Ronde','Sekoteng','STMJ','Wedang Angsle'];
+var jajanan = ['Apem','Cenil','Jenang','Klepon','Kucur','Putu','Lupis','Nagasari','Gethuk Telo'];
+var gabungan = makanan.concat(minuman, jajanan);
